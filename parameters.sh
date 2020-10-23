62578ae51a30f0196a85659824e423eb79d4cb94
Marilzon <maxmaril@hotmail.com>
2020-10-23 10:00:02 -0300

    Update: Update fila names and Include params.sh example

---
+++ parameters.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+# $0 - Name of Script
+# $# - Total of Parameters
+# $* - All inserted parameters
+# $1-9 - Each of the parameters
+
+echo "The script $0 has been $# parameters"
+
+echo "The parameters received has: $*"
+echo ""
+echo "The parameter \$1 = $1"
+echo "The parameter \$2 = $2"
+echo "The parameter \$3 = $3"
+echo "The parameter \$4 = $4"
