86e29bcba5eb76a725b89e0f80b31bd09d229ef8
Marilzon <maxmaril@hotmail.com>
2020-10-23 15:12:23 -0300

    Create: dir exercicies, add first lesson

---
+++ exercicies/sum.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#########################################################
+#							#
+# sum.sh - input Two numbers,sum and show result	#
+#							#
+# Author: Marilzon de Sousa(maxmaril@hotmail.com)	#
+#							#
+# Usage from bash: . sum.sh				#
+#							#
+#########################################################
+
+read -p "Input the first NUMBER: " a
+read -p "Input the second NUMBER: " b
+
+echo $((a + b))
+
