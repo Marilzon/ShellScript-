19fc15fc898284ec84cac0e658f918c3b8481edd
Marilzon <maxmaril@hotmail.com>
2020-10-27 23:05:13 -0300

    Feature: Script to allow permission chmod +x fast.

---
+++ chmodx.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# chmodx.sh 
+# Author: Marilzon de Sousa(maxmaril@hotmail.com)
+# Description:  script to change permission to all users read, write and execute the script
+# Usage: .chmodx.sh
+clear 
+
+chmod +x *.sh*
+echo "Permission to read write and edit done!"
