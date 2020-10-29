d29c24c08cdfe0e3bbd40eb97e3057fd66544c8e
Marilzon <maxmaril@hotmail.com>
2020-10-29 03:51:02 -0300

    Feature: lesson to learn debug command bash

---
+++ lessons/debugMode.sh
@@ -0,0 +1,12 @@
+#!bin/bash/
+
+# Name: debigMode.sh
+# Author: Marilzon de Sousa(maxmaril@hotmail.com)
+# Description: Script to show bash in mode debugger
+# Usage: . debugMode.sh
+clear
+set -xv # use -x or -v or -xv, for variables cases
+echo "Hello developer!"
+read -p "Whats your name? " NAME 
+echo
+echo "Welcome $NAME, have a nice code!"
